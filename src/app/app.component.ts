3961f1de79d263f8b0c06aea217a68736dc3cccb
Alfonso Madrid <alfonso.madrid.dev@gmail.com>
2022-09-17 21:25:20 +0200

    Uncomment implementation with hefesto-kairos service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { StationsService } from './stations.service';
+
 import { Component,  OnDestroy, OnInit } from '@angular/core';
+import { StationsService } from './services/stations.service';
 
 @Component({
   selector: 'app-root',
@@ -8,41 +9,42 @@ import { Component,  OnDestroy, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'afrodita';
-  stations: any[] = [
-    {
-      _id: {
-        $oid: "6321a90407ddd45232f83b82"
-      },
-      createdDate: "14/09/22 10:12h",
-      location: {
-          name: "Casa Consuelo",
-          indoor: true,
-          city: "Vegadeo",
-          latitude: 43.463297,
-          longitude: -7.051219
-      },
-      measurements: [
-        {date: "14/09/22 15:05h", temperature: 28.0, humidity: 73.0},
-        {date: "14/09/22 15:05h", temperature: 27.60000038, humidity: 74.0},
-        {date: "14/09/22 15:11h", temperature: 27.29999924, humidity: 75.0},
-      ]
-    }
-  ];
+  stations: any[] = [];
+  // stations: any[] = [
+  //   {
+  //     _id: {
+  //       $oid: "6321a90407ddd45232f83b82"
+  //     },
+  //     createdDate: "14/09/22 10:12h",
+  //     location: {
+  //         name: "Casa Consuelo",
+  //         indoor: true,
+  //         city: "Vegadeo",
+  //         latitude: 43.463297,
+  //         longitude: -7.051219
+  //     },
+  //     measurements: [
+  //       {date: "14/09/22 15:05h", temperature: 28.0, humidity: 73.0},
+  //       {date: "14/09/22 15:05h", temperature: 27.60000038, humidity: 74.0},
+  //       {date: "14/09/22 15:11h", temperature: 27.29999924, humidity: 75.0},
+  //     ]
+  //   }
+  // ];
   stationSubscription: any;
 
   constructor(private stationsService: StationsService) {}
 
   ngOnInit(): void {
-    // this.stationsService.getAll().subscribe((data: any[])=>{
-    //   console.log('stations', data);
-    //   this.stations = data;
-    // });
+    this.stationsService.getAll().subscribe((data: any[])=>{
+      console.log('stations', data);
+      this.stations = data;
+    });
   }
 
   ngOnDestroy(): void {
-    // if (this.stationSubscription) {
-    //   this.stationSubscription.unsubscribe();
-    // }
+    if (this.stationSubscription) {
+      this.stationSubscription.unsubscribe();
+    }
   }
 
   getLastMeasurement(stationId: string): any {
