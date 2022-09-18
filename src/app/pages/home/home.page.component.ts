c8854807b1bfb9477bb4ddc3fde6f5e7b72950f5
Alfonso Madrid <alfonso.madrid.dev@gmail.com>
2022-09-18 21:57:04 +0200

    Uncommented service implementation

--- src/app/pages/home/home.page.component.ts
+++ src/app/pages/home/home.page.component.ts
@@ -7,41 +7,41 @@ import { StationsService } from 'src/app/services/stations.service';
   styleUrls: ['./home.page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-// stations: any[] = [];
-stations: any[] = [
-  {
-    _id: {
-      $oid: "6321a90407ddd45232f83b82"
-    },
-    createdDate: "14/09/22 10:12h",
-    location: {
-        name: "Casa Consuelo",
-        indoor: true,
-        city: "Vegadeo",
-        latitude: 43.463297,
-        longitude: -7.051219
-    },
-    measurements: [
-      {date: "14/09/22 15:05h", temperature: 28.0, humidity: 73.0},
-      {date: "14/09/22 15:05h", temperature: 27.60000038, humidity: 74.0},
-      {date: "14/09/22 15:11h", temperature: 27.29999924, humidity: 75.0},
-    ]
+  stations: any[] = [];
+  // stations: any[] = [
+  //   {
+  //     _id: {
+  //       $oid: "6321a90407ddd45232f83b82"
+  //     },
+  //     createdDate: "14/09/22 10:12h",
+  //     location: {
+  //         name: "Casa Consuelo",
+  //         indoor: true,
+  //         city: "Vegadeo",
+  //         latitude: 43.463297,
+  //         longitude: -7.051219
+  //     },
+  //     measurements: [
+  //       {date: "14/09/22 15:05h", temperature: 28.0, humidity: 73.0},
+  //       {date: "14/09/22 15:05h", temperature: 27.60000038, humidity: 74.0},
+  //       {date: "14/09/22 15:11h", temperature: 27.29999924, humidity: 75.0},
+  //     ]
+  //   }
+  // ];
+  stationSubscription: any;
+
+  constructor(private stationsService: StationsService) {}
+
+  ngOnInit(): void {
+    this.stationsService.getAll().subscribe((data: any[])=>{
+      console.log('stations', data);
+      this.stations = data;
+    });
   }
-];
-// stationSubscription: any;
 
-constructor(private stationsService: StationsService) {}
-
-ngOnInit(): void {
-  // this.stationsService.getAll().subscribe((data: any[])=>{
-  //   console.log('stations', data);
-  //   this.stations = data;
-  // });
-}
-
-ngOnDestroy(): void {
-  // if (this.stationSubscription) {
-  //   this.stationSubscription.unsubscribe();
-  // }
-}
+  ngOnDestroy(): void {
+    if (this.stationSubscription) {
+      this.stationSubscription.unsubscribe();
+    }
+  }
 }
